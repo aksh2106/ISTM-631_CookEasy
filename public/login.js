ff9965ab1c165e2658f84c6f4946b4dd5831c372
Alok Shah <alokshah@main-10-230-49-14.tamulink.tamu.edu>
2019-04-05 11:23:57 -0500

    remember me/forgot password

--- public/login.js
+++ public/login.js
@@ -1,32 +1,69 @@
+ var config = {
+    apiKey: "AIzaSyAoy5P0YCilP18UoyeyWtf53xviVZSxtAY",
+    authDomain: "cookeasy-36dd0.firebaseapp.com",
+    databaseURL: "https://cookeasy-36dd0.firebaseio.com",
+    projectId: "cookeasy-36dd0",
+    storageBucket: "cookeasy-36dd0.appspot.com",
+    messagingSenderId: "337324868275"
+  };
+  firebase.initializeApp(config);
+
+
 var app = angular.module('cookEasyApp', ['ngRoute', 'firebase']);
 
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    
+    var path = "/home.html";
+    window.location.href = path;
+
+  } 
+});
+
+
 app.controller('LoginCtrl', ['$scope', '$window', '$firebaseArray',
-    function($scope, $window , $firebaseArray) {
+    function($scope, $window, $firebaseArray) {
 
 
-    var usersRef = new Firebase('https://cookeasy-36dd0.firebaseio.com');
+    var usersRef = firebase.database().ref();
 
     $scope.users = $firebaseArray(usersRef);
 
      $scope.login = function () {
 
-          
-                var login = false;
 
-                angular.forEach($scope.users, function(user) {
-                 
-                 if(user.email === $scope.Email && user.password === $scope.Password)
-                 {
-                    var path = "/home.html";
-                    $window.location.href = path;
-                    login = true;
-                 }
+            var email = $scope.Email;
+            var password = $scope.Password;
 
+           
+
+            firebase.auth().setPersistence('local')
+            .then(function() {
+            
+            firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+                var path = "/home.html";
+                $window.location.href = path;
+            }).catch(function(error) {
+                window.alert('Incorrect Login Credentials!');
+            });
+           
+             })
+            .catch(function(error) {
+             // Handle Errors here.
+             var errorCode = error.code;
+             var errorMessage = error.message;
             });
 
-            if(!login)
-            window.alert('Incorrect Login Credentials!');
-
         };
+
+
+    $scope.resetPassword = function () {
+
+        var path = "/password_reset.html";
+        window.location.href = path;
+
+    };  
 
 }]);
--- public/signup.js
+++ public/signup.js
@@ -1,11 +1,31 @@
+ var config = {
+    apiKey: "AIzaSyAoy5P0YCilP18UoyeyWtf53xviVZSxtAY",
+    authDomain: "cookeasy-36dd0.firebaseapp.com",
+    databaseURL: "https://cookeasy-36dd0.firebaseio.com",
+    projectId: "cookeasy-36dd0",
+    storageBucket: "cookeasy-36dd0.appspot.com",
+    messagingSenderId: "337324868275"
+  };
+  firebase.initializeApp(config);
+
+
 var app = angular.module('cookEasyApp', ['ngRoute', 'firebase']);
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    
+    var path = "/home.html";
+    window.location.href = path;
+
+  } 
+});
+
 app.controller('AddUserCtrl', ['$scope', '$window', '$firebaseArray',
-    function($scope, $window , $firebaseArray) {
+    function($scope, $window, $firebaseArray) {
 
-    var usersRef = new Firebase('https://cookeasy-36dd0.firebaseio.com');
+     var usersRef = firebase.database().ref();
 
-    $scope.users = $firebaseArray(usersRef);
+     $scope.users = $firebaseArray(usersRef);
 
      $scope.addUser = function () {
 
@@ -25,8 +45,28 @@ app.controller('AddUserCtrl', ['$scope', '$window', '$firebaseArray',
 
             });
 
-            var path = "/home.html";
-            $window.location.href = path;
+            var email = $scope.Email;
+            var password = $scope.Password;
+
+             
+
+            firebase.auth().setPersistence('local')
+            .then(function() {
+            
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+                var path = "/home.html";
+                $window.location.href = path;
+            }).catch(function(error) {
+                window.alert('Error creating account');
+            });
+           
+             })
+            .catch(function(error) {
+             // Handle Errors here.
+             var errorCode = error.code;
+             var errorMessage = error.message;
+            });
+
         };
 
 }]);
