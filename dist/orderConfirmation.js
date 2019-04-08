50df61bdb09e385a9570f1b0fd20b9b605999123
aksh2106 <akshat.verma2106@gmail.com>
2019-04-07 22:39:33 -0500

    recipe page persistance

--- dist/orderConfirmation.js
+++ dist/orderConfirmation.js
@@ -39,4 +39,5 @@ angular.module('cookEasy.orderConfirmation', ['ngRoute', 'firebase'])
   });
 
   firebase.database().ref().child('/ShoppingCart/Cart1').remove();
+  firebase.database().ref().child('/TempTable').remove();
 }])
